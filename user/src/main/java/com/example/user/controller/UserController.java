ddd8a7ad820e98b26ef2461caea62f08cbf44925
qiaowan <30237108+qiaowanw@163.com>
2021-12-21 21:52:12 +0800

    更新config配置文件

--- user/src/main/java/com/example/user/controller/UserController.java
+++ user/src/main/java/com/example/user/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.user.controller;
 import com.example.user.domain.User;
 import com.example.user.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,9 +15,12 @@ public class UserController {
     @Autowired
     private UserMapper userMapper;
 
+    @Value("${dbconfig}")
+    private String username;
+
     @RequestMapping("/users")
     public List<User> users(){
-        System.err.println("user1");
+        System.err.println("user1: " + username);
         return userMapper.findAll();
     }
 }
